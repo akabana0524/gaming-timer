fe81099d6bcf76a245e35dac81a291d52b9eb1b3
akabana0524 <akabana0524@gmail.com>
2020-12-30 03:03:59 +0000

    setup nuxt project

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
-[![Gitpod ready-to-code](https://img.shields.io/badge/Gitpod-ready--to--code-blue?logo=gitpod)](https://gitpod.io/#https://github.com/akabana0524/gaming-timer)
-
 # gaming-timer
+
+## Build Setup
+
+```bash
+# install dependencies
+$ npm install
+
+# serve with hot reload at localhost:3000
+$ npm run dev
+
+# build for production and launch server
+$ npm run build
+$ npm run start
+
+# generate static project
+$ npm run generate
+```
+
+For detailed explanation on how things work, check out [Nuxt.js docs](https://nuxtjs.org).
---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    <el-button v-on:click="test()">test</el-button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  data() {
+    return {};
+  },
+
+  methods: {
+    async test() {
+      const result = await Notification.requestPermission();
+      if (result === "granted") {
+        var notifTitle = "title";
+        var notifBody = "body";
+        var options = {
+          body: notifBody
+        };
+        new Notification("test", options);
+      }
+    }
+  }
+});
+</script>
+
+<style>
+</style>
